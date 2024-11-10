307fd0806403b823d07e3c5023d9ea26748663f3
justinpakzad <justinpak303@gmail.com>
2024-11-10 14:25:29 -0500

    error handling added for fast api app.

--- app/fast_api/main.py
+++ app/fast_api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from annoy import AnnoyIndex
 from pydantic import BaseModel
 from .utils import load_mappings, get_n_components
@@ -7,7 +7,7 @@ from pathlib import Path
 
 class RecRequest(BaseModel):
     url: str
-    n_recs: int = 7
+    n_recs: int = 5
 
 
 app = FastAPI()
@@ -20,20 +20,31 @@ async def root():
 
 @app.post("/recommend")
 async def get_top_n_recommendations(request: RecRequest):
+    # try to load necessary mappings/files
     ann_file = Path(__file__).resolve().parents[1] / "ann_files" / "discogs_rec.ann"
-    mappings = load_mappings()
-    f = get_n_components()
-    t = AnnoyIndex(f, "angular")
-    t.load(str(ann_file))
+    if not ann_file.exists():
+        raise HTTPException(
+            status_code=404,
+            detail="Annoy index file not found, please make sure it exists!",
+        )
+    try:
+        mappings = load_mappings()
+        f = get_n_components()
+        t = AnnoyIndex(f, "angular")
+        t.load(str(ann_file))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail="Failed to load mappings/annoy index files"
+        )
     release_id = int(request.url.split("release/")[-1].split("-")[0])
     item_idx = mappings.get("release_id_to_idx").get(release_id)
     nearest_indices = t.get_nns_by_item(
-        item_idx, n=request.n_recs + 5, include_distances=False
+        item_idx, n=request.n_recs + 25, include_distances=False
     )
     seen_artists = set()
     recs = []
-    for count, idx in enumerate(nearest_indices, start=1):
-        if count >= request.n_recs + 1:
+    for idx in nearest_indices:
+        if len(recs) >= request.n_recs:
             break
         rel_id = mappings.get("idx_to_release_id").get(idx)
         url = f"https://www.discogs.com/release/{rel_id}"
